a6cdedf2a46a49f9649679bd5fd934bc878ae60a
gaspid <70648535+gaspid@users.noreply.github.com>
2021-02-19 18:52:10 +0300

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
+ History
+ 1 contributor
+21 lines (21 sloc)  551 Bytes
+  
 module.exports = function check(str, bracketsConfig) {
-  const bracketPairs = bracketsConfig.map(function (val) {
-      return val.join("");
-  });
-
-  let reducedStr = str;
-  let prevStr;
-  do{
-      prevStr = reducedStr;
-      bracketPairs.forEach(function (val) {
-          reducedStr = reducedStr.replace(val,"");
-      });
-  } while (reducedStr.length < prevStr.length);
-
-  return reducedStr.length === 0;
+   if (str.length % 2 === 1) return false;
+  while (str.length > 0) {
+    let flag = true;
+    for (let i=0; i<str.length; i++)
+     {
+      for (let j in bracketsConfig) {
+        if (str[i] === bracketsConfig[j][0] && flag)
+          if (str[i + 1] === bracketsConfig[j][1]) {
+            let a = str.slice(0, i);
+            let b = str.slice(i+2, str.length);
+            str = a + b;
+            flag = false;
+          }
+      }
+    }
+    if(flag)
+       return false;
+  }
+  return true;
 }
