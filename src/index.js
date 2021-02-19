78f07842db327cad465121b9343d6599b4c266e6
gaspid <gaspid194@gmail.com>
2021-02-19 18:29:06 +0300

    feat: implement min, max and avg functions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
 }
+
+exports.min = function min (array) {
+  if ((array === undefined) || (array.length === 0)){
+      return 0;
+  }
+return Math.min.apply(null, array);
+}
+
+exports.max = function max (array) {
+  if ((array === undefined) || (array.length === 0)){
+      return 0;
+  }
+  return Math.max.apply(null, array);;
+}
+
+exports.avg = function avg (array) {
+  if ((array === undefined) || (array.length === 0)){
+      return 0;
+  }
+  return array.reduce((accumulator, currentValue) => accumulator + currentValue)/array.length;
+}
